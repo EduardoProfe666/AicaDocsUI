0363794902c7deb1091ac7fc27b5358fe84dfda7
LilyRosa <135471998+LilyRosa@users.noreply.github.com>
2024-04-20 16:38:33 -0400

    Usuario en detalles de documentos y botón eliminar en nomenclador de proceso

--- Repositories/ApiData/Dto/Downloads/DownloadDto.cs
+++ Repositories/ApiData/Dto/Downloads/DownloadDto.cs
@@ -7,7 +7,7 @@ public class DownloadDto
     public int Id { get; set; }
     public DateTimeOffset DateOfDownload { get; set; } = DateTimeOffset.UtcNow;
     public required Format Format { get; set; }
-    public required string Username { get; set; }
+    public required string UserEmail { get; set; }
 
     public required int DocumentId { get; set; }
     public required int ReasonId { get; set; }
